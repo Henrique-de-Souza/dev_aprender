950592ac485199a5c00cb98cec1d11bec24c7a5c
Henrique de Souza <enriquedesuu@gmail.com>
2023-11-08 08:48:21 -0300

    primeiro desafio do curso da DEV APRENDER

---
+++ app.py
@@ -0,0 +1,15 @@
+# Desafio 1
+
+# Copie e cole as seguintes linhas de código para
+# seu editor de código e descubra qual é o tipo de
+# cada um desses dados
+
+lista = [
+    variavel_1 := '25.87',
+    variavel_2 := True,
+    variavel_3 := 'Bom dia!',
+    variavel_4 := 15
+]
+
+for i in lista:
+    print(f'O tipo da variavel {i} é do tipo: {type(i)}')
